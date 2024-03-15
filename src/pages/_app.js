be3bf6801b9f145c856af604747489783bc0b136
huoyijie <yijie.huo@foxmail.com>
2024-03-15 13:31:12 +0800

    refactor mv applayout to bottlejs

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,15 +3,17 @@ import { NextIntlClientProvider } from 'next-intl'
 import { useRouter } from 'next/router'
 import Head from 'next/head'
 import { ConfigProvider, App, theme } from 'antd'
-import useLayout from '@/components/hooks/useLayout'
-import { AppSWRConfig } from '@authink/bottlejs'
+import { useLayout, AppSWRConfig } from '@authink/bottlejs'
 import { useState } from 'react'
+import { AppstoreOutlined, DashboardOutlined } from '@ant-design/icons'
+import copyright from '@/lib/copyright'
 
 const timeZone = process.env.NEXT_PUBLIC_TIME_ZONE
+const authnRoutes = ['/', '/apps']
 
 export default function MyApp({ Component, pageProps }) {
   const router = useRouter()
-  const Layout = useLayout()
+  const Layout = useLayout(() => authnRoutes.includes(router.pathname))
   const [currentTheme, setCurrentTheme] = useState('light')
   const toggleTheme = () => {
     setCurrentTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'))
@@ -32,7 +34,24 @@ export default function MyApp({ Component, pageProps }) {
         }}
       >
         <App>
-          <Layout currentTheme={currentTheme} toggleTheme={toggleTheme}>
+          <Layout
+            logoText="INK"
+            currentTheme={currentTheme}
+            toggleTheme={toggleTheme}
+            menuItems={[
+              {
+                key: 'dashboard',
+                Icon: DashboardOutlined,
+                route: '/',
+              },
+              {
+                key: 'apps',
+                Icon: AppstoreOutlined,
+                route: '/apps',
+              },
+            ]}
+            copyright={copyright()}
+          >
             <Head>
               <meta name="description" content="Ink Admin" />
               <meta
