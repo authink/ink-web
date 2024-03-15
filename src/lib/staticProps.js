955cf0ce47685b2ce0bf7bbc754d72105a64a1dc
huoyijie <yijie.huo@foxmail.com>
2024-03-15 15:35:50 +0800

    refactor mv componets locales to bottlejs

--- src/lib/staticProps.js
+++ src/lib/staticProps.js
@@ -1,9 +1,12 @@
+import { locales } from '@authink/bottlejs'
+
 export default async function staticProps(locale, page) {
   return {
     props: {
       messages: {
         ...(await import(`@/locales/${page}/${locale}.json`)).default,
         ...(await import(`@/locales/component/${locale}.json`)).default,
+        ...(await locales(locale)),
       },
     },
   }
