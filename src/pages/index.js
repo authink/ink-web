d49d0184d381d5d55dd87caa15a7d8eb34b66e24
huoyijie <yijie.huo@foxmail.com>
2024-03-15 17:52:44 +0800

    dashboard call api

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,18 +2,31 @@ import Head from 'next/head'
 import { Button } from 'antd'
 import staticProps from '@/lib/staticProps'
 import { useTranslations } from 'next-intl'
+import { Loading, useQuery } from '@authink/bottlejs'
+import { setImmediate } from '@authink/commonjs'
+import { message } from 'antd'
 
 export default function Dashboard() {
   const t = useTranslations()
+  const { data, error, isLoading, isValidating } = useQuery({
+    path: 'admin/dashboard',
+  })
+
+  if (isLoading || isValidating) {
+    return <Loading />
+  }
+
+  if (error) {
+    setImmediate(() => message.error(error.message))
+  }
+
   return (
     <>
       <Head>
         <title>{t('dashboard')}</title>
       </Head>
 
-      <div>
-        <Button type="primary">{t('welcome')}</Button>
-      </div>
+      <div>{data && <Button type="primary">{t('welcome')}</Button>}</div>
     </>
   )
 }
