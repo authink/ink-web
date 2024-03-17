59f4353e9173e13718ec321e7401d1b4ab02e310
huoyijie <yijie.huo@foxmail.com>
2024-03-17 23:34:55 +0800

    support delete token feature

--- src/pages/tokens.js
+++ src/pages/tokens.js
@@ -4,7 +4,10 @@ import { useDataSource } from '@authink/bottlejs'
 import { usePagination } from '@authink/bottlejs'
 import { useColumns } from '@authink/bottlejs'
 import { useSuccess } from '@authink/bottlejs'
+import { useMutation } from '@authink/bottlejs'
 import { Loading, useQuery } from '@authink/bottlejs'
+import { ignoreError } from '@authink/commonjs'
+import { http } from '@authink/commonjs'
 import { Button, Flex } from 'antd'
 import { Tooltip } from 'antd'
 import { Popconfirm } from 'antd'
@@ -12,14 +15,15 @@ import { Table } from 'antd'
 import { useTranslations } from 'next-intl'
 import Head from 'next/head'
 
-const path = 'admin/tokens'
+const tokensPath = 'admin/tokens'
 
 export default function Tokens() {
   const t = useTranslations()
   const showSuccess = useSuccess()
   const { pagination, limit, offset } = usePagination()
+  const path = `${tokensPath}?limit=${limit}&offset=${offset}`
   const { data, isLoading, isValidating } = useQuery({
-    path: `${path}?limit=${limit}&offset=${offset}`,
+    path,
     options: {
       revalidateOnFocus: false,
     },
@@ -33,6 +37,10 @@ export default function Tokens() {
     'accountId',
     'createdAt',
   ])
+  const { trigger: delToken, isMutating } = useMutation({
+    path,
+    method: http.DELETE,
+  })
 
   if (isLoading || isValidating) {
     return <Loading />
@@ -45,10 +53,12 @@ export default function Tokens() {
       <Flex wrap="wrap" gap="small">
         <Popconfirm
           title={t('confirmDelete')}
-          onConfirm={() => {
-            showSuccess(t('deleteSucceed'))
-            console.log(token)
-          }}
+          onConfirm={() =>
+            ignoreError(async () => {
+              await delToken({ id: token.id })
+              showSuccess(t('deleteSucceed'))
+            })
+          }
         >
           <Tooltip title={t('delete')} placement="bottom">
             <Button
@@ -56,6 +66,7 @@ export default function Tokens() {
               shape="circle"
               danger
               icon={<DeleteOutlined />}
+              disabled={isMutating}
             />
           </Tooltip>
         </Popconfirm>
